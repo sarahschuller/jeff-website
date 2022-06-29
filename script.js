51d74ea7d04e6739483d6a39936120565d276d4e
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-06-29 19:59:39 -0400

    create function for scrolling

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('It Works!')
+function scrollInto(id) {
+    console.log('you clicked it')
+    const element = document.querySelector(id);
+    element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+    });
+}
