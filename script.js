67f622f9c34d3dae674b0138e642e096873c23ab
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-21 20:04:16 -0400

    testing

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function scrollInto(id) {
-    console.log('you clicked it')
-    const element = document.querySelector(id);
+function scrollInto(contact) {
+    const element = document.getElementById('contact');
     element.scrollIntoView({
         behavior: "smooth",
         block: "start",
         inline: "nearest",
     });
+    console.log('scrolling!!');
 }
