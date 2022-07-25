8622d32ff4aecfaa95966b43a2d54553f400345d
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-07-25 15:42:12 -0400

    add animation for scroll event

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function scrollInto(contact) {
-    const element = document.getElementById('contact');
-    element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-        inline: "nearest",
-    });
-    console.log('scrolling!!');
+// function scrollInto(contact) {
+//     const element = document.getElementById('contact');
+//     element.scrollIntoView({
+//         behavior: "smooth",
+//         block: "start",
+//         inline: "nearest",
+//     });
+//     console.log('scrolling!!');
+// }
+
+window.onscroll = function() {onScroll()};
+
+function onScroll() {
+    if(document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.querySelector('.extra-img-wrapper').className = 'slideIn'
+    }
 }
